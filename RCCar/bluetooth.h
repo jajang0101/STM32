7257b80c15b0306f1bf62ec47c9ca3e86ce9b36b
jajang0101 <97460317+jajang0101@users.noreply.github.com>
2026-01-19 20:22:52 +0900

    Delete RCCar/bluetooth.h

--- RCCar/bluetooth.h
+++
@@ -1,8 +0,0 @@
-#define STM32F030x8
-#include "Inc/stm32f030x8.h"
-#define FCK 8000000U
-#define BAUDRATE 9600U
-
-void Bluetooth_Init(unsigned baudrate);
-int Bluetooth_RXNE();
-uint8_t Bluetooth_ReadKey();
